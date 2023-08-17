e6da30cf0a92868ed5b88bd6a7183af29b9c6c3b
snigbar <snigbar@outlook.com>
2023-08-17 18:37:33 +0600

    profile page added

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { Box, useMediaQuery } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Navbar from '../../components/components/Navbar';
+import UserWidget from '../../widgets/UserWidget';
+import FriendsWidget from '../../widgets/FriendsWidget';
+import MyPostWidgets from '../../widgets/MyPostWidgets';
+import PostsWidet from '../../widgets/PostsWidet';
 
 const ProfilePage = () => {
+  const [user, setUser] = useState(null)
+  const {userId} = useParams()
+  const token = useSelector((state) => state.token);
+  const bigScreen = useMediaQuery("(min-width:1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:5000/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+ 
+  if(!user) return null
+
   return (
-    <div>ProfilePage</div>
+    <Box>
+    <Navbar />
+    <Box
+      width="100%"
+      padding="2rem 6%"
+      display={bigScreen ? "flex" : "block"}
+      gap="2rem"
+      justifyContent="center"
+    >
+      <Box flexBasis={bigScreen ? "26%" : undefined}>
+        <UserWidget userId={userId} picturePath={user.picturePath} />
+        <Box m="2rem 0" />
+        <FriendsWidget userId={userId} />
+      </Box>
+      <Box
+        flexBasis={bigScreen ? "42%" : undefined}
+        mt={bigScreen ? undefined : "2rem"}
+      >
+        <MyPostWidgets picturePath={user.picturePath} />
+        <Box m="2rem 0" />
+        <PostsWidet userId={userId} isProfile />
+      </Box>
+    </Box>
+  </Box>
   )
 }
 
